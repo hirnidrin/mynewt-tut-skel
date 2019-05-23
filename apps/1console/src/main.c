affebe55a10fccd23e19f5e005246414c862baed
hirnidrin <info@itsu.ch>
2019-05-23 22:16:49 +0200

    Add console tutorial.

---
+++ apps/1console/src/main.c
@@ -0,0 +1,97 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <assert.h>
+#include <string.h>
+
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+#ifdef ARCH_sim
+#include "mcu/mcu_sim.h"
+#endif
+
+/* global, blinky loop counter */
+static volatile int g_task1_loops;
+
+/* For LED toggling */
+int g_led_pin;
+
+/* blinky timer callout */
+static struct os_callout blinky_callout;
+
+/*
+ * Callback function for timer events, toggles the LED pin.
+ */
+static void
+timer_ev_cb(struct os_event *ev)
+{
+    assert(ev != NULL);
+
+    ++g_task1_loops;
+    hal_gpio_toggle(g_led_pin);
+
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC);
+}
+
+/*
+ * Init the timer thing, before entering the infinite loop in main().
+ */
+static void
+init_timer(void)
+{
+    /* setup the blinky timer callout struct */
+    os_callout_init(&blinky_callout, os_eventq_dflt_get(), timer_ev_cb, NULL);
+
+    /* and start the timer */
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC);
+}
+
+/**
+ * main
+ *
+ * The main task for the project. This function initializes packages,
+ * and then blinks the BSP LED in a loop.
+ *
+ * @return int NOTE: this function should never return!
+ */
+int
+main(int argc, char **argv)
+{
+    int rc;
+
+#ifdef ARCH_sim
+    mcu_sim_parse_args(argc, argv);
+#endif
+
+    sysinit();
+
+    g_led_pin = LED_BLINK_PIN;
+    hal_gpio_init_out(g_led_pin, 1);
+    init_timer();
+
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+    assert(0);
+
+    return rc;
+}
+
